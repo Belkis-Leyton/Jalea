7f61f25556de43e0503d7d33d3abfda98664bcb0
BelkisLeyton <belkis.leyton950@std.uni.edu.ni>
2025-06-22 15:11:42 -0600

    Modificacion Belkis.java

--- Belkis.java
+++ Belkis.java
@@ -1,8 +1,8 @@
 public class Belkis{
     public static void main(String[] args) {
         String nombre="natti";
-        int a =5;
-        int b=3;
+        int a =7;
+        int b=9;
         int suma=a+b;
         System.out.println("hola"+ nombre);
         System.out.println("la suma de" + a +" + " + b +"es" + suma);
